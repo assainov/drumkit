a8835e2414f210571d07d3fafdb24952e3f4f421
Ilyas Assainov <iliyas.assainov@gmail.com>
2018-10-11 18:34:40 +0400

    app ready

--- dist/app.js
+++ dist/app.js
@@ -2,11 +2,9 @@ const audios = document.querySelectorAll('audio');
 
 
 document.addEventListener('keydown', (e) => {
-    e.preventDefault();
 
-    
     audios.forEach(function(audio) {
-        if(e.code.toString() === audio.dataset.key) {
+        if(e.keyCode.toString() === audio.dataset.key) {
             audio.currentTime = 0;
             audio.play();
             
@@ -14,4 +12,26 @@ document.addEventListener('keydown', (e) => {
         }
     });
 
+});
+
+document.addEventListener('keyup', (e) => {
+    
+    audios.forEach(function(audio) {
+        if(e.keyCode.toString() === audio.dataset.key) {
+            document.getElementById(e.keyCode).parentElement.classList.remove('playing');
+        }
+    });
+});
+
+
+document.addEventListener('click', (e) => {
+
+    if (e.target.nodeName === 'A') {
+        audios.forEach(function(audio) {
+            if(e.target.id === audio.dataset.key) {
+                audio.currentTime = 0;
+                audio.play();
+            }
+        });
+    }
 });
